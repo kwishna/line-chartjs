e97b8858b6b28e279d0728315af1f21837620167
kwishna <kwishna@gmail.com>
2024-10-14 02:57:30 +0530

    schema updated

--- graphql-backend/models/dbSchema.js
+++ graphql-backend/models/dbSchema.js
@@ -1,4 +1,45 @@
-const {DataTypes} = require('sequelize');
+// const {DataTypes} = require('sequelize');
+
+// // DB table schema
+// const dbSchema = {
+//     test_name: {
+//         type: DataTypes.STRING,
+//         allowNull: false
+//     },
+//     test_outcome: {
+//         type: DataTypes.STRING,
+//         allowNull: false
+//     },
+//     test_tags: {
+//         type: DataTypes.STRING,
+//         allowNull: true
+//     },
+//     test_execution_date: {
+//         type: DataTypes.STRING,
+//         allowNull: false
+//     },
+//     test_environment: {
+//         type: DataTypes.STRING,
+//         allowNull: false
+//     },
+//     error_message: {
+//         type: DataTypes.TEXT,
+//         allowNull: true
+//     },
+//     test_category: {
+//         type: DataTypes.STRING,
+//         allowNull: false
+//     },
+//     test_case_id: {
+//         type: DataTypes.STRING,
+//         allowNull: false
+//     }
+// }
+
+// module.exports = dbSchema;
+
+const { DataTypes } = require('sequelize');
+const moment = require('moment');
 
 // DB table schema
 const dbSchema = {
@@ -15,8 +56,12 @@ const dbSchema = {
         allowNull: true
     },
     test_execution_date: {
-        type: DataTypes.DATE,
-        allowNull: false
+        type: DataTypes.STRING, // Keep as STRING to store formatted date
+        allowNull: false,
+        get() {
+            const value = this.getDataValue('test_execution_date');
+            return value ? moment(value, 'DD-MM-YYYY').format('DD-MM-YYYY') : null; // Format on retrieval
+        }
     },
     test_environment: {
         type: DataTypes.STRING,
@@ -34,6 +79,7 @@ const dbSchema = {
         type: DataTypes.STRING,
         allowNull: false
     }
-}
+};
 
 module.exports = dbSchema;
+
